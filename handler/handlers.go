e17931fb752a343de86cf1465d99795f3317f18c
grak0s <devops.mao@gmail.com>
2020-12-12 18:14:52 -0500

    lee y elimina tweet

--- handler/handlers.go
+++ handler/handlers.go
@@ -22,6 +22,7 @@ func Manejadores() {
 	router.HandleFunc("/verperfil", middlew.ChequeoDB(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
 	router.HandleFunc("/modificarperfil", middlew.ChequeoDB(middlew.ValidoJWT(routers.ModificarPerfil))).Methods("PUT")
 	router.HandleFunc("/tweet", middlew.ChequeoDB(middlew.ValidoJWT(routers.GraboTweet))).Methods("POST")
+	router.HandleFunc("/leoTweet", middlew.ChequeoDB(middlew.ValidoJWT(routers.LeoTweets))).Methods("GET")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
